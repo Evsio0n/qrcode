aefc32e0e81b5d6fc6b8899b4d88420d6ec472e6
evsio0n <bbq2001820@gmail.com>
2020-07-20 22:00:28 +0800

    Update

---
+++ README.md
@@ -0,0 +1,6 @@
+# A simple restful Qr-code HTTP backend written by go
+Simply start http backend just use `./qrcode start`
+
+## Usage 
+We can use ./qrcode start to get the http server run as daemon 
+Use browser or `wget` at http://127.0.0.1:8081?w=200&h=200&c=word-content` to get content api.
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"./module/library/log"
 	"./module/library/qrcode"
+	"context"
 	"fmt"
+	"github.com/medivh-jay/daemon"
+	"github.com/spf13/cobra"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+	"syscall"
 )
 
 func qr(w http.ResponseWriter, r *http.Request) {
@@ -28,7 +33,7 @@ func qr(w http.ResponseWriter, r *http.Request) {
 		if key == "c" {
 			c = strings.Join(v, "")
 			count++
-			oc=1
+			oc = 1
 		}
 		if key == "w" {
 			ws = strings.Join(v, "")
@@ -52,29 +57,82 @@ func qr(w http.ResponseWriter, r *http.Request) {
 		content := qrcode.ToPng(c, wi, h)
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "Image/png")
-		w.Header().Set("Content-Length",strconv.Itoa(len(content.Bytes())))
-		_,_=w.Write(content.Bytes())
+		w.Header().Set("Content-Length", strconv.Itoa(len(content.Bytes())))
+		_, _ = w.Write(content.Bytes())
 		count = 0
 		problem = 0
-	}else if count==1&&oc==1{
+	} else if count == 1 && oc == 1 {
 		content := qrcode.NoToPng(ws)
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "Image/png")
-		w.Header().Set("Content-Length",strconv.Itoa(len(content.Bytes())))
-		_,_=w.Write(content.Bytes())
+		w.Header().Set("Content-Length", strconv.Itoa(len(content.Bytes())))
+		_, _ = w.Write(content.Bytes())
 		count = 0
 		problem = 0
-	}else {
+	} else {
 		w.WriteHeader(403)
 		_, _ = fmt.Fprintf(w, "Error pram set!")
 		count = 0
 		problem = 0
 	}
 }
-func main() {
+func httpServerPid() {
 	http.HandleFunc("/", qr)                        //处理 push
-	err := http.ListenAndServe("0.0.0.0:8787", nil) //端口为8787
+	err := http.ListenAndServe("0.0.0.0:8081", nil) //端口为8787
 	if err != nil {
 		log.Debug("ListenAndServe: ", err) //监听端口
+	}
+}
+
+// HTTPServer http 服务器示例
+type HTTPServer struct {
+	http *http.Server
+	cmd  *cobra.Command
+}
+
+// PidSavePath pid保存路径
+func (httpServer *HTTPServer) PidSavePath() string {
+	return "./"
+}
+
+// Name pid文件名
+func (httpServer *HTTPServer) Name() string {
+	return "http"
+}
+
+// SetCommand 从 daemon 获得 cobra.Command 对象
+
+func (httpServer *HTTPServer) Start() {
+	httpServerPid()
+}
+func (httpServer *HTTPServer) Stop() error {
+	err := httpServer.http.Shutdown(context.Background())
+	return err
+}
+
+// Restart 重启web服务前关闭http服务
+func (httpServer *HTTPServer) Restart() error {
+	err := httpServer.Stop()
+	return err
+}
+
+func main() {
+	// 自定义输出文件
+	out, _ := os.OpenFile("./http.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	err, _ := os.OpenFile("./http_err.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+
+	// 初始化一个新的运行程序
+	proc := daemon.NewProcess(new(HTTPServer)).SetPipeline(nil, out, err)
+	proc.On(syscall.SIGTERM, func() { fmt.Println("a custom signal") })
+	// 示例,多级命令服务
+	// 这里的示例由于实现了 Command 接口, 所以这里会出现 flag test 不存在的情况, 实际情况, 每一个 worker 都应该是唯一的
+	// 不要共享一个 worker 对象指针
+	daemon.GetCommand().AddWorker(proc).AddWorker(proc)
+	// 示例,主服务
+	daemon.Register(proc)
+
+	// 运行
+	if rs := daemon.Run(); rs != nil {
+		log.Info(rs)
 	}
 }
